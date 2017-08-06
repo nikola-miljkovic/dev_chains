19a9dfbc117f6c00fa95aa8c037625e4d61e119a
nikola-miljkovic <milja1337@gmail.com>
2017-08-06 18:17:34 +0200

    DOSTA FALI

--- data_processor_service/src/main/scala/org/nmiljkovic/DataProcessorService.scala
+++ data_processor_service/src/main/scala/org/nmiljkovic/DataProcessorService.scala
@@ -5,14 +5,37 @@ import com.redis._
 import scala.collection.immutable.HashMap
 
 class DataProcessorService {
+  private val CLEAN_REPO_SET = "clean:repo:set"
+  private val CLEAN_REPO_PREFIX = "clean:repo"
+
   private val r = new RedisClient("localhost", 6379)
   private val topRepos = TakeTopRepos(40)
-  ClearRepoPaths()
-    .toList.sortBy(- _._2).foreach(k => println(s"${k._1} : ${k._2}"))
+  private val cleanRepos = ClearRepoPaths()
 
-  println("-----------------")
-  println("")
-  topRepos.foreach(k => println(s"${k._1} : ${k._2}"))
+  private val newCleanRepoSet = cleanRepos.keys.toSet
+  private val oldCleanRepoSet = r.smembers(CLEAN_REPO_SET).orNull.collect { case Some(key) => key }
+
+  println(s"Starting transaction for updating sets.")
+
+  r.pipeline { c =>
+    c.del(CLEAN_REPO_SET)
+    cleanRepos.toList.sortBy(- _._2).foreach { pair =>
+      c.sadd(CLEAN_REPO_SET, pair._1)
+      c.set(s"$CLEAN_REPO_PREFIX:${pair._1}", pair._2)
+    }
+  }
+
+  println(s"Transaction completed.")
+  println(s"Total clean repositories: ${newCleanRepoSet.size}")
+  println(s"Starting cleanup.")
+
+  private val reposToDelete = oldCleanRepoSet.diff(newCleanRepoSet)
+
+  reposToDelete.foreach { key =>
+    r.del(key)
+  }
+
+  println(s"Cleanup completed. Total keys removed: ${reposToDelete.size}")
 
   private def CollectKeys(keyPrefix: String) = r.keys(s"$keyPrefix*").orNull
       .collect { case Some(key) => key }
@@ -28,7 +51,7 @@ class DataProcessorService {
       .toMap
   }
 
-  private def removeLanguagesBelowThreshold(acc: HashMap[String, Int], pair: (String, Int)) = {
+  private def removeLanguagesBelowThreshold(acc: HashMap[String, Int], pair: (String, Int)): HashMap[String, Int] = {
     val (path, count) = pair
     val cleanPath = path.split(":")
       .foldLeft(List[String]())((acc, lang) => {
@@ -45,7 +68,7 @@ class DataProcessorService {
     }
   }
 
-  private def ClearRepoPaths() = {
+  private def ClearRepoPaths(): HashMap[String, Int] = {
     val KEY_PREFIX = "repo:path:"
     val KEY_PREFIX_SIZE = KEY_PREFIX.length
 
